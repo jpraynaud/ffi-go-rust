5e18a0e3722490d85b65ba0aa8db8e377972f00e
JP Raynaud <jp.raynaud@gmail.com>
2022-02-25 18:11:45 +0100

    Updated cgo LDLFLAGS

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -3,7 +3,7 @@ package main
 // NOTE: There should be NO space between the comments and the `import "C"` line.
 
 /*
-#cgo LDFLAGS: -L../../rust/target/release/ -lrustlib
+#cgo LDFLAGS: -L../../rust/target/release/ -lrustlib -ldl
 #include "./rustlib.h"
 */
 import "C"
